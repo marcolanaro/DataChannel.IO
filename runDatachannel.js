af8cdde296447581b800071697abc12eb9e74688
Michael-Schaer <mdschaer@gmail.com>
2014-04-02 16:53:41 +0200

    Update runDatachannel.js

--- runDatachannel.js
+++ runDatachannel.js
@@ -4,7 +4,7 @@ var RedisStore = require("connect-redis")(connect);
 var redis = require("redis");
 
 var dc = require('datachannel.io').listen(server, {
-        redis: {port: 6379, host: "localhost", options: {  }},
+        redis: {port: 6379, host: "localhost", options: { static:false }},
 });
 
 dc.addNameSpace("preciseioServer", {});
